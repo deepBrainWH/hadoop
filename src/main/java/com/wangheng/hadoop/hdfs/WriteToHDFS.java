bf38ea0f44f9c53865605ea67f4e4ad70a0e9afd
wangheng <673602165@qq.com>
2019-03-14 21:11:39 +0800

    list file and directory

---
+++ src/main/java/com/wangheng/hadoop/hdfs/WriteToHDFS.java
@@ -0,0 +1,27 @@
+package com.wangheng.hadoop.hdfs;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.*;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class WriteToHDFS {
+
+    public static void main(String[] args) {
+        Configuration conf = new Configuration();
+        conf.set("fs.defaultFS", "hdfs://localhost:9000/");
+        try {
+            FileSystem fileSystem = FileSystem.get(URI.create("hdfs://localhost:9000"), conf, "wangheng");
+            LocalFileSystem localFileSystem = FileSystem.getLocal(conf);
+            FileStatus[] fileStatuses = fileSystem.listStatus(new Path("/"));
+            for (FileStatus fileStatuse : fileStatuses) {
+
+                System.out.println( fileStatuse.getPath().toString() +
+                        "\t\t\t" + "\033[32m" +(fileStatuse.isDirectory()?"DIR":"FILE") + "\033[0m");
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
